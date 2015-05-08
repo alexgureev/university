06904e2c621f8c59f3bc1bf6b8a8358cc3e71034
barif <w3db@yandex.ru>
2015-05-08 23:30:21 +0300

    Data insertation and saving into file

--- struct-and-algorithm/lab_02/lab_02.cpp
+++ struct-and-algorithm/lab_02/lab_02.cpp
@@ -11,16 +11,10 @@
 
 using namespace std;
 
-struct City
+struct Speciality
 {
 	char title[10];
-	Institute institutes[10];
-};
-
-struct Institute
-{
-	char title[10];
-	Faculty faculties[10];
+	int places;
 };
 
 struct Faculty
@@ -29,13 +23,17 @@ struct Faculty
 	Speciality specialities[10];
 };
 
-struct Speciality
+struct Institute
 {
 	char title[10];
-	int places;
+	Faculty faculties[10];
 };
 
-int i, j;
+struct City
+{
+	char title[10];
+	Institute institutes[10];
+};
 
 FILE * fp;
 
@@ -63,18 +61,107 @@ FILE * fp;
 		користувачем спеціальність виділено найбільше місць. 
  */
 
-void input();
+void setData();
+void setCities(FILE * fp);
+void setInstitutes(City city);
+void setFaculties(Institute institute);
+void setSpecialities(Faculty faculty);
 
-void input()
+void setData()
 {
+	if (fp != NULL) {
+		remove("fp.dat");
+	}
 
+	fopen_s(&fp, "fp.dat", "wb");
+
+	setCities(fp);
+	
+	fclose(fp);
+}
+
+void setCities(FILE * fp)
+{
+	City cities[10];
+	int i = 0;
+	char answer;
+
+	do
+	{
+		cout << "City title: " << endl;
+		cin >> cities[i].title;
+		setInstitutes(cities[i]);
+
+		cout << "One more city? y/n" << endl;
+		answer = _getch();
+		
+		fwrite(&cities[i], sizeof(City), 1, fp);
+		
+		i++;
+	} while (answer != 'n' || i >= 10);
+}
+
+void setInstitutes(City city)
+{
+	char answer;
+	int i = 0;
+
+	do
+	{
+		cout << "Insitute title: " << endl;
+		cin >> city.institutes[i].title;
+		setFaculties(city.institutes[i]);
+
+		cout << "One more institute? y/n" << endl;
+		answer = _getch();
+		i++;
+
+	} while (answer != 'n' || i >= 10);
+}
+
+void setFaculties(Institute institute)
+{
+	char answer;
+	int i = 0;
+
+	do
+	{
+		cout << "Faculty title: " << endl;
+		cin >> institute.faculties[i].title;
+		setSpecialities(institute.faculties[i]);
+
+		cout << "One more faculty? y/n" << endl;
+		answer = _getch();
+		i++;
+
+	} while (answer != 'n' || i >= 10);
+}
+
+void setSpecialities(Faculty faculty)
+{
+	char answer;
+	int i = 0;
+
+	do
+	{
+		cout << "Speciality title: " << endl;
+		cin >> faculty.specialities[i].title;
+
+		cout << "Places: " << endl;
+		cin >> faculty.specialities[i].places;
+
+		cout << "One more speciality? y/n" << endl;
+		answer = _getch();
+		i++;
+
+	} while (answer != 'n' || i >= 10);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	system("cls");
 	cout << " MENU" << endl;
-	cout << "1. Input" << endl;
+	cout << "1. set" << endl;
 	cout << "2. Read data from file" << endl;
 	cout << "3. " << endl;
 	cout << "4. " << endl;
@@ -93,7 +180,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	switch (key)
 	{
-		case 1: { break; }
+		case 1: { setData();  break; }
 		case 2: { break; }
 		case 3: { break; }
 		case 4: { break; }
