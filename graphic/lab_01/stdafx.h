db4459700658de1979707f3c44922921f1c12b49
barif <w3db@yandex.ru>
2015-05-22 18:02:11 +0300

    Algorithms clipping Lines Cohen-Sutherland

---
+++ graphic/lab_01/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int  V_CSclip(float *x0, float *y0, float *x1, float *y1);
