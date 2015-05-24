6e8912bcd7a50300db3dbd6698fd8383255f29d0
barif <w3db@yandex.ru>
2015-05-24 18:03:29 +0300

    second task refactoring

--- graphic/lab_02/stdafx.h
+++ graphic/lab_02/stdafx.h
@@ -4,14 +4,8 @@
 
 #include <stdio.h>
 #include <tchar.h>
-#include <windows.h>
-#include <iostream>
-#include <cmath>
 #include <conio.h>
-#include <cstdlib>
 
 using namespace std;
 
-#define PI 3.14
-
-void Bresenham(int x1, int y1, int const x2, int const y2);
+void inputData();
