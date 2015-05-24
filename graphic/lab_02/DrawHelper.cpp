5aeb648cdb0298811860e3820d4e91ba1f0e5dbb
barif <w3db@yandex.ru>
2015-05-24 18:59:37 +0300

    finish second task

--- graphic/lab_02/DrawHelper.cpp
+++ graphic/lab_02/DrawHelper.cpp
@@ -48,32 +48,31 @@ void DrawHelper::releaseContext()
 
 void DrawHelper::drawLine(int x1, int y1, int const x2, int const y2)
 {
-	int delta_x(x2 - x1);
+	int dX = x2 - x1;
+	int dY = y2 - y1;
 
-	signed char const ix((delta_x > 0) - (delta_x < 0));
-	delta_x = std::abs(delta_x) << 1;
+	signed int const ix = (dX > 0) ? 1 : -1;
+	signed int const iy = (dY > 0) ? 1 : -1;
 
-	int delta_y(y2 - y1);
-
-	signed char const iy((delta_y > 0) - (delta_y < 0));
-	delta_y = std::abs(delta_y) << 1;
-
+	dX = abs(dX) << 1;
+	dY = abs(dY) << 1;
+	
 	this->drawPixel(x1, y1, this->getRedColor());
 
-	if (delta_x >= delta_y)
+	if (dX >= dY)
 	{
 
-		int error(delta_y - (delta_x >> 1));
+		int error(dY - (dX >> 1));
 
 		while (x1 != x2)
 		{
 			if ((error >= 0) && (error || (ix > 0)))
 			{
-				error -= delta_x;
+				error -= dX;
 				y1 += iy;
 			}
 
-			error += delta_y;
+			error += dY;
 			x1 += ix;
 
 			this->drawPixel(x1, y1, this->getRedColor());
@@ -82,17 +81,17 @@ void DrawHelper::drawLine(int x1, int y1, int const x2, int const y2)
 	else
 	{
 
-		int error(delta_x - (delta_y >> 1));
+		int error(dX - (dY >> 1));
 
 		while (y1 != y2)
 		{
 			if ((error >= 0) && (error || (iy > 0)))
 			{
-				error -= delta_y;
+				error -= dY;
 				x1 += ix;
 			}
 
-			error += delta_x;
+			error += dX;
 			y1 += iy;
 
 			this->drawPixel(x1, y1, this->getRedColor());
