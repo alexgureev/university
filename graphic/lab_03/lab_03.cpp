0697f3d8cdf1c617628c98228d0b8142b56a081b
barif <w3db@yandex.ru>
2015-05-24 17:08:32 +0300

    complete third task

--- graphic/lab_03/lab_03.cpp
+++ graphic/lab_03/lab_03.cpp
@@ -1,56 +1,41 @@
 #include "stdafx.h"
+#include "DrawHelper.h"
 
-HWND myconsole = GetConsoleWindow();
-HDC mydc = GetDC(myconsole);
-COLORREF COLOR = RGB(0, 0, 0);
+DrawHelper * helper = new DrawHelper();
+int x, y, r;
+
+void inputData()
+{
+	puts("Enter x: ");
+	cin >> x;
+	puts("Enter y: ");
+	cin >> y;
+	puts("Enter r: ");
+	cin >> r;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	system("color f0");
-	int ix1, iy1, ir1;
-	puts("Enter x1: ");
-	cin >> ix1;
-	puts("Enter y1: ");
-	cin >> iy1;
-	puts("Enter r: ");
-	cin >> ir1;
-	cout << "\nPlotting a circle with the center at (" << ix1 << "; " << iy1 << ") and radius of " << ir1 << "\n\n";
-	system("pause");
-	system("cls");
-	while (!_kbhit())  {
-		DrawCircle(ix1, iy1, ir1);
-	}
-	ReleaseDC(myconsole, mydc);
-	cin.ignore();
+	helper->setTerminalColor();
+	inputData();
 
+	cout << endl
+		<< "After your click, we will draw circle"
+		<< endl 
+		<< " with center in O ("
+		<< x
+		<< "; "
+		<< y
+		<< ") and r="
+		<< r
+		<< endl << endl;
+
+	helper->pause();
+	helper->clearScreen();
+
+	while (helper->wait())  {
+		helper->drawCircle(x, y, r);
+	}
+	
 	return 0;
-}
-
-void DrawCircle(int xc, int yc, int radius) {
-	int x = 0;
-	int y = radius;
-	int delta = 2 - 2 * radius;
-	int error = 0;
-	while (y >= 0)
-	{
-		SetPixel(mydc, xc + x, yc + y, COLOR);
-		SetPixel(mydc, xc - x, yc + y, COLOR);
-		SetPixel(mydc, xc + x, yc - y, COLOR);
-		SetPixel(mydc, xc - x, yc - y, COLOR);
-		error = 2 * (delta + y) - 1;
-		if (delta < 0 && error <= 0) {
-			++x;
-			delta += 2 * x + 1;
-			continue;
-		}
-		error = 2 * (delta - x) - 1;
-		if (delta > 0 && error > 0) {
-			--y;
-			delta += 1 - 2 * y;
-			continue;
-		}
-		++x;
-		delta += 2 * (x - y);
-		--y;
-	}
 }
--- graphic/lab_03/stdafx.h
+++ graphic/lab_03/stdafx.h
@@ -4,9 +4,8 @@
 
 #include <stdio.h>
 #include <tchar.h>
-#include<windows.h>
-#include <iostream>
 #include <conio.h>
+
 using namespace std;
 
-void DrawCircle(int xc, int yc, int radius);
+void inputData();
